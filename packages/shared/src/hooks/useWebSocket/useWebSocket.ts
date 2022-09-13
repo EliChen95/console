48dce1f0f83273686dba60dd4482fc4e6db0de1a
donniean <acmilank22@gmail.com>
2022-09-13 18:37:51 +0800

    feat: useWebSocket

---
+++ packages/shared/src/hooks/useWebSocket/useWebSocket.ts
@@ -0,0 +1,37 @@
+import { useState, useEffect } from 'react';
+
+import type { WebSocketClientOptions } from '../../utils';
+import { WebSocketClient, urlHelper } from '../../utils';
+
+const { getWebSocketProtocol, getClusterUrl } = urlHelper;
+
+interface UseWebSocketOptions<T> extends WebSocketClientOptions<T> {
+  url: string;
+}
+
+export type { UseWebSocketOptions };
+
+export function useWebSocket<T>({ url, ...websocketClientOptions }: UseWebSocketOptions<T>) {
+  const protocol = getWebSocketProtocol(window.location.protocol);
+  const { host } = window.location;
+  const pathname = getClusterUrl(`/${url}`);
+
+  const [message, setMessage] = useState<T>();
+
+  const { onmessage, ...rest } = websocketClientOptions;
+  const websocketClient = new WebSocketClient<T>(`${protocol}://${host}${pathname}`, {
+    ...rest,
+    onmessage: (data, ev) => {
+      setMessage(data);
+      if (typeof onmessage === 'function') {
+        onmessage(data, ev);
+      }
+    },
+  });
+
+  useEffect(() => {
+    return websocketClient.close(true);
+  }, []);
+
+  return { websocketClient, message };
+}
